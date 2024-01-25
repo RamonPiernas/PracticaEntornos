765cd4354afa66161a3c695e606600365486890c
alum <alum@a41e03-19.infor.com>
2024-01-25 19:43:51 +0100

    clase añadida

---
+++ src/PaqueteRamon/ClaseRamon.java
@@ -0,0 +1,15 @@
+
+package PaqueteRamon;
+
+import java.util.Scanner;
+
+public class ClaseRamon {
+
+    public static void main(String[] args) {
+        Scanner teclado= new Scanner(System.in);
+        System.out.println("saludos secuaces");
+        
+       
+    }
+    
+}
